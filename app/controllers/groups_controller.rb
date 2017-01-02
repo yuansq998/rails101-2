622fab6c76895bba072efcd4524ad86f23817d7c
yuansq998 <yuansq998@163.com>
2017-01-02 12:17:27 +0800

    add post and hook group/user relation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create，:edit, :update, :destroy ]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy ]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
@@ -32,7 +32,7 @@ class GroupsController < ApplicationController
     end
   end
   def destroy
-  
+
     @group.destroy
     flash[:alert] = "Group Deleted"
     redirect_to groups_path
